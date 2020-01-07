a99f711bede9996ffe1b9bc1af0138cfe565032c
Davi Holanda <daviht7@gmail.com>
2020-01-06 21:00:32 -0300

    estrutura do projeto no app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from 'react';
 
 function App() {
+
+  const comments = [
+    "comment 1","comment 2"
+ ]
+
+  const [comment,setComment] = useState(comments)
+
+  function sendComment(c) {
+    setComment([...comment,c])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* new comment */}
+      <div>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <button onClick={() => sendComment("olá 1")}>Enviar</button>
+      </div>
+      {/* comments */}
+      <div>
+        {comment.map(c => (
+          <div>{c}</div>
+      ))}
+      </div>
     </div>
   );
 }
